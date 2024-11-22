270991df6cee3f72978c16cb5d91077eefefb3be
leejieun <mariewldms@naver.com>
2024-11-23 01:19:41 +0900

    Feat: like-posts, like-user N:1 mapping

--- src/main/java/com/jojoldu/book/springboot/domain/user/User.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -1,16 +1,24 @@
 package com.jojoldu.book.springboot.domain.user;
 
 import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import com.jojoldu.book.springboot.domain.like.Like;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class User extends BaseTimeEntity {
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Like> like=new ArrayList<>();
+    //final: 객체 참조만 고정, 내부 데이터는 변경 가능
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
