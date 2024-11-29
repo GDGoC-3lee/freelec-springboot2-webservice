0a880becb7a529dfc29832719f41fba3789c0fd5
ruddmslee <dlruddms326@naver.com>
2024-11-29 17:13:12 +0900

    Feat: Service 추가/수정

--- src/main/java/com/jojoldu/book/springboot/domain/like/LikeRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/like/LikeRepository.java
@@ -1,11 +1,9 @@
 package com.jojoldu.book.springboot.domain.like;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
-public interface LikeRepository extends JpaRepository<Like,Long> {
-
+public interface LikeRepository extends JpaRepository<Like_Table,Long> {
+    boolean existsByPostsAndUser(Posts posts, User user);
 }
